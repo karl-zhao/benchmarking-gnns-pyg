fb4606410a14334f3fcba79bbcd9d5e931ab4c22
karl <zhaowentao163@163.com>
2020-11-16 10:33:22 +0800

    modify data

--- data/data.py
+++ data/data.py
@@ -1,13 +1,9 @@
 """
     File to load dataset based on user control from main file
 """
-from data.superpixels import SuperPixDataset
 from data.molecules import *
-from data.TUs import TUsDataset
 from data.SBMs import SBMsDataset, SBMsDatasetpyg
-from data.TSP import TSPDataset
-from data.COLLAB import COLLABDataset
-from data.CSL import CSLDataset
+
 from data.planetoids import PlanetoidDataset
 from data.ogbn import ogbnDatasetpyg
 
@@ -19,34 +15,15 @@ def LoadData(DATASET_NAME, use_node_embedding = False, framework = 'dgl'):
         ; dataset object
     """
     # handling for MNIST or CIFAR Superpixels
-    if DATASET_NAME == 'MNIST' or DATASET_NAME == 'CIFAR10':
-        return SuperPixDataset(DATASET_NAME)
     
     # handling for (ZINC) molecule dataset
     if DATASET_NAME == 'ZINC':
         return MoleculeDataset(DATASET_NAME) if 'dgl' == framework else MoleculeDatasetpyg(DATASET_NAME)
 
-    # handling for the TU Datasets
-    TU_DATASETS = ['ENZYMES', 'DD', 'PROTEINS_full']
-    if DATASET_NAME in TU_DATASETS: 
-        return TUsDataset(DATASET_NAME)
-
     # handling for SBM datasets
     SBM_DATASETS = ['SBM_CLUSTER', 'SBM_PATTERN']
     if DATASET_NAME in SBM_DATASETS: 
         return SBMsDatasetpyg(DATASET_NAME) if 'pyg' == framework else SBMsDataset(DATASET_NAME)
-    
-    # handling for TSP dataset
-    if DATASET_NAME == 'TSP':
-        return TSPDataset(DATASET_NAME)
-
-    # handling for COLLAB dataset
-    if DATASET_NAME == 'OGBL-COLLAB':
-        return COLLABDataset(DATASET_NAME, framework)
-
-    # handling for the CSL (Circular Skip Links) Dataset
-    if DATASET_NAME == 'CSL': 
-        return CSLDataset(DATASET_NAME)
 
     if DATASET_NAME in ['Cora', 'Citeseer', 'Pubmed']:
         return PlanetoidDataset(DATASET_NAME, use_node_embedding = use_node_embedding)
